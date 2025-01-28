4e6e37a54bc8161305ecc83c0a629befc2ad55b1
jumanah khlid albisher <jumanahkhalid45@gmail.com>
2025-01-28 11:03:40 +0300

    Initial Commit

---
+++ Disney_animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Disney_animation
+//
+//  Created by jumanah khalid albisher on 28/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Disney_animation/Disney_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Disney_animationApp.swift
+//  Disney_animation
+//
+//  Created by jumanah khalid albisher on 28/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Disney_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
